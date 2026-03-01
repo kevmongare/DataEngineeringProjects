77c89f0a4f25035667fde5d4f023550036f09c8e
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2026-03-02 00:08:12 +0300

    update:Day 2 update on for loops and while loops

---
+++ section1_python_foundations/day_3_loops/for_loops.py
@@ -0,0 +1,6 @@
+#this is a script to shoow loops to be specific a for loop
+
+list_of_numbers = [0,1,2,3,4,5,6,7,8,9]
+print("This is the multiplication table for 9 \n From 0-9:")
+for num in list_of_numbers:	
+	print(9*num)
---
+++ section1_python_foundations/day_3_loops/while_loop.py
@@ -0,0 +1,7 @@
+#this is a script to show loops to be specific the while loop
+
+num = 10
+while num < 19:
+	print(num)
+	num+=1
+
