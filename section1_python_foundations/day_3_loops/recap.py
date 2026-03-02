f8554117655640550198e481ce709e9822c803a4
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2026-03-02 13:48:28 +0300

    feat: Addition of a recap file on dat 3 on loops

---
+++ section1_python_foundations/day_3_loops/recap.py
@@ -0,0 +1,18 @@
+"""
+Today we want to work on a recap 
+for for loops 
+And for while loops
+"""
+
+print("recap on both for loops and while loops")
+number_list = [1,2,3,4,5,6,7,8,9]
+number = 0
+for num in number_list:
+    print(num*10)
+    #Helps redo the task for the whole lost
+
+#for loops are used 
+while number <= 5:
+    print(number)
+    number+=1
+    #works on a condition as long as the condition is true
